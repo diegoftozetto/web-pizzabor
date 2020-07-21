ef3859b928e3336061027dbdf33d7c92cf85248f
Diego Tozetto <diegotozetto@gmail.com>
2020-07-21 20:46:58 -0300

    Add evento submit e verificação dos parâmetros de entrada

--- js/script.js
+++ js/script.js
@@ -3,4 +3,36 @@ $(document).ready(function () {
 		e.preventDefault();
 		$(this).tab('show');
 	});
+
+	// Produtos
+	$("#form-product").submit(function (e) {
+		e.preventDefault();
+		var me = $(this);
+		addProduct(me.attr('action'), me.attr('method'), me.serializeArray());
+	});
 });
+
+///
+/// Produtos
+///
+
+// Adicionar Produto
+function addProduct(action, method, data) {
+	var dataJson = {}, flag = true;
+
+	data.forEach(element => {
+		if (element.name != 'url') {
+			if (!element.value || typeof element.value == undefined || element.value == null) {
+				flag = false;
+				return;
+			}
+		}
+		dataJson[element.name] = element.value; 
+	});
+
+	if (!flag) {
+		alert("Falha ao processar requisição. Parâmetros Inválidos.");
+	} else {
+		alert("Produto Adicionado.");
+	}
+}
