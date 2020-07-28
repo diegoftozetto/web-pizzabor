c1510fbbc77f0c75740c0b6991f1e9888108011f
Diego Tozetto <diegotozetto@gmail.com>
2020-07-28 19:54:35 -0300

    Carregamento dos dados da requisição em uma lista

--- js/script.js
+++ js/script.js
@@ -56,6 +56,8 @@ function addProduct(action, method, data) {
 				//Limpar elementos do form
 				$("#form-product input, #form-product textarea").val('');
 				$("#form-product select").prop('selectedIndex', 0);
+
+				listProducts();
 			},
 			error: (result) => {
 				switch (result.status) {
@@ -79,7 +81,45 @@ function listProducts() {
 		contentType: "application/json",
 		url: "https://api-pizzabor.herokuapp.com/products",
 		success: function (result) {
-			console.log(result);
+			$("#div-products").html("");
+			if (result.length > 0) {
+				var currentCategorie = null;
+				result.forEach((element, index) => {
+					// Titulo da categoria
+					if (element.categorie != currentCategorie) {
+						currentCategorie = element.categorie;
+						$("#div-products").append("<div class='alert alert-secondary mt-4' role='alert'><h4 class='alert-heading'>" + currentCategorie + "</h4></div>");
+					}
+
+					// Card
+					$("#div-products").append("<div class='card mt-2' id='" + index + "'></div>");
+					// Titulo do card
+					$("#div-products" + " #" + index).append("<h5 class='card-header'>" + element.name + "</h5>"); 
+					//Corpo do card
+					$("#div-products" + " #" + index).append("<div class='card-body'></div>");
+
+					if (element.url)
+						$("#div-products" + " #" + index + " .card-body").append("<img src='" + element.url + "' alt='Imagem do Produto' id='img-pizza' class='img-thumbnail rounded float-left mr-4'>");
+					else
+						$("#div-products" + " #" + index + " .card-body").append("<img src='../../img/logo-pizzabor.png' alt='Imagem do Produto' id='img-pizza' class='img-thumbnail rounded float-left mr-4'>");
+
+					$("#div-products" + " #" + index + " .card-body").append("<p><b>Descrição: </b>" + element.description + "</p>");
+
+					if (element.categorie == "Pizza Salgada" || element.categorie == "Pizza Doce")
+						$("#div-products" + " #" + index + " .card-body").append("<p><b>Preço:</b> Broto - R$" + (element.price * 12).toFixed(2) + " | Média - R$" + (element.price * 18).toFixed(2) + " | Grande - R$" + (element.price * 24).toFixed(2) + " | Família - R$" + (element.price * 32).toFixed(2) + "</p>");
+					else if (element.categorie == "Calzone")
+						$("#div-products" + " #" + index + " .card-body").append("<p><b>Preço:</b> Média - R$" + (element.price * 22).toFixed(2) + " | Grande - R$" + (element.price * 31).toFixed(2) + "</p>");
+					else
+						$("#div-products" + " #" + index + " .card-body").append("<p><b>Preço:</b> R$ " + element.price + "</p>");
+
+					//Rodape do card
+					$("#div-products" + " #" + index).append("<div class='card-footer text-muted'></div>");
+					$("#div-products" + " #" + index + " .card-footer").append("<a><button class='btn btn-info btn-sm'>Editar</button></a>");
+					$("#div-products" + " #" + index + " .card-footer").append("<a onClick=\"deleteProduct('" + element._id + "');\"><button class='btn btn-danger btn-sm ml-2'>Remover</button></a>");
+				});
+			} else {
+				$("#div-products").append("<div class='alert alert-warning' role='alert'>Nenhum Produto Cadastrado.</div>");
+			}
 		},
 		error: function (result) {
 			$("#div-products").html("");
